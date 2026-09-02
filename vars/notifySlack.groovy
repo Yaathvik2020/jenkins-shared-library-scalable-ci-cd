570290fe888ad2a1c50633526cdf1d7e82f37245
Yaathvik2020 <chinnayaathvik@gmail.com>
2026-09-02 23:12:50 +0530

    Update notifySlack.groovy

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -3,4 +3,5 @@ def call(Map config) {
     def color = (config.status == 'SUCCESS') ? 'good' : 'danger'
     slackSend(channel: '#ci-cd-alerts', color: color, message: "${config.appName} build ${config.status}: ${env.BUILD_URL}")
     */
+    sh " echo stuck involved"
 }
