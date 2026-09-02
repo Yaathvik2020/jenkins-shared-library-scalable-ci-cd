11968eedca91720d7d2b3eb7127f2c83ee58c5f8
Yaathvik2020 <chinnayaathvik@gmail.com>
2026-09-02 23:12:09 +0530

    Comment out docker build and push commands
    Comment out docker build and push commands for debugging.

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,8 +1,9 @@
 def call(Map config) {
     def image = "${config.dockerhubUser}/${config.imageName}:${config.tag}"
-    sh "docker build -t ${image} ."
+    //sh "docker build -t ${image} ." 
     withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh "echo \$PASS | docker login -u \$USER --password-stdin"   // no registry URL needed for Docker Hub
-        sh "docker push ${image}"
+        sh " echo login success"
+       // sh "docker push ${image}"
     }
 }
