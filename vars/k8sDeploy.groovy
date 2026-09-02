3e2612992c461fe1f5378062f4981b62d4278ae5
Yaathvik2020 <chinnayaathvik@gmail.com>
2026-09-02 23:07:19 +0530

    Comment out deployment command in k8sDeploy.groovy
    Comment out the kubectl command for deploying images.

--- vars/k8sDeploy.groovy
+++ vars/k8sDeploy.groovy
@@ -1,7 +1,9 @@
 def call(Map config) {
+    /*   k8s need to deploy 
     sh """
         kubectl set image deployment/${config.imageName} \
         ${config.imageName}=${config.registry}/${config.imageName}:${config.tag} \
         -n ${config.namespace}
     """
+    */
 }
