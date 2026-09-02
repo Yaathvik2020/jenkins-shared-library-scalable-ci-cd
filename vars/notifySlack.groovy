02100df4ee3b2e38e4f3f193c0b56d5cef6e3ec6
Yaathvik2020 <chinnayaathvik@gmail.com>
2026-09-02 23:09:09 +0530

    Comment out notifySlack function implementation
    Comment out the implementation of the notifySlack function.

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,4 +1,6 @@
 def call(Map config) {
+    /* need  stack  set up to use this 
     def color = (config.status == 'SUCCESS') ? 'good' : 'danger'
     slackSend(channel: '#ci-cd-alerts', color: color, message: "${config.appName} build ${config.status}: ${env.BUILD_URL}")
+    */
 }
